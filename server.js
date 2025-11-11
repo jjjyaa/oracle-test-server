8a4a74d10fd674b613d0cd95aaff847203f154b1
jjjyaa <as960503@naver.com>
2025-11-11 09:22:30 +0900

    Update server.js

--- server.js
+++ server.js
@@ -3,17 +3,16 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Oracle JSON 테스트 서버 ');
-});
-
 app.post('/api/test', (req, res) => {
-  console.log('Oracle에서 받은 데이터:', req.body);
+  console.log('오라클에서 받은 데이터 건수 :', Array.isArray(req.body) ? req.body.length : 1);
+  console.log(req.body); // JSON 전체 출력
+
   res.json({
-    message: '서버에서 JSON 응답 완료!',
-    received: req.body
+    message: '서버에서 JSON 수신 완료',
+    receivedCount: Array.isArray(req.body) ? req.body.length : 1
   });
 });
 
 app.listen(3000, () => console.log('Server running...'));
+
 
